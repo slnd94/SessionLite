c4e5be280fc34f765da94b80a4db17fc0240dc1c
Simon Davies <slndavies@gmail.com>
2021-11-10 14:04:00 -0500

    update

--- client-web/next.config.js
+++ client-web/next.config.js
@@ -4,5 +4,8 @@ module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+  },
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.API_BASE_URL
   }
 }
