2ec6a6348b37309935ce0efabfc426b9bbcd3af9
Simon Davies <slndavies@gmail.com>
2021-11-14 16:54:53 -0500

    auth through context and bootstrap form

---
+++ client-web/context/AuthContext.js
@@ -0,0 +1,92 @@
+// import { AsyncStorage } from 'react-native';
+import createDataContext from './createDataContext';
+// import trackerApi from '../api/tracker';
+// import { navigate } from '../navigationRef';
+import api from '../utils/api';
+import { useRouter } from 'next/router';
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
+    case 'signin':
+      return { errorMessage: '', token: action.payload };
+    case 'clear_error_message':
+      return { ...state, errorMessage: '' };
+    case 'signout':
+      return { token: null, errorMessage: '' };
+    default:
+      return state;
+  }
+};
+
+const tryLocalSignin = dispatch => async () => {
+  const token = localStorage.authToken;
+  if (token) {
+    dispatch({ type: 'signin', payload: token });
+    // navigate('TrackList');
+  } else {
+    // navigate('Signup');
+  }
+};
+
+const clearErrorMessage = dispatch => () => {
+  dispatch({ type: 'clear_error_message' });
+};
+
+const signup = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signup', { email, password });
+    localStorage.authToken = response.data.accessToken;
+    // await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signin', payload: response.data.token });
+    router.push('/');
+    // navigate('TrackList');
+  } catch (err) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with sign up'
+    });
+  }
+};
+
+const signin = dispatch => async ({ email, password }) => {
+  try {
+    console.log('tohere1')
+    // const response = await trackerApi.post('/signin', { email, password });
+    // await AsyncStorage.setItem('token', response.data.token);
+    const response = await api({
+      method: 'post',
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/authentication`,
+      params: {
+        strategy: 'local', 
+        email,
+        password
+      }
+    });
+
+    localStorage.authToken = response.data.accessToken;
+    dispatch({ type: 'signin', payload: response.data.accessToken });
+    // router.push('/');
+    // navigate('TrackList');
+  } catch (err) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with sign in'
+    });
+  }
+};
+
+const signout = dispatch => async () => {
+  await AsyncStorage.removeItem('token');
+  dispatch({ type: 'signout' });
+  navigate('loginFlow');
+};
+
+const { Provider, Context } = createDataContext(
+  authReducer,
+  { signin, signout, signup, clearErrorMessage, tryLocalSignin },
+  { token: null, errorMessage: '' }
+);
+
+export { Provider, Context };
--- client-web/pages/_app.js
+++ client-web/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.scss'
+import '../styles/bootstrap.scss'
+// import 'bootstrap/dist/css/bootstrap.css';
+import { Provider as AuthProvider } from '../context/AuthContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
