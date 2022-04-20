b1499e7eebe7d7bfefc85ac4858ac20bc1ace02e
Simon Davies <slndavies@gmail.com>
2022-04-20 13:38:35 -0400

    cart title

--- client-web/pages/user/cart.js
+++ client-web/pages/user/cart.js
@@ -2,13 +2,22 @@ import Layout from '../../components/user/Layout'
 import { Context as AuthContext } from '../../context/AuthContext';
 import { useEffect, useContext } from 'react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
 import styles from '../../styles/Layout.module.scss'
 
 export default function Cart() {
+  const { t } = useTranslation('common');
+
   return (    
     <div>
       <Layout>
-        <div>cart content here</div>
+        <div>
+          <div className="row mt-3 mt-md-0 ms-0 ms-md-3">
+            <div className="col-md-8">
+              <h5 className={'title'}>{t('user.Your Cart')}</h5>
+            </div>
+          </div>
+        </div>
       </Layout>
     </div>
   )
