1964e97f5905bb739679705d291b729b44df0581
Simon Davies <slndavies@gmail.com>
2022-04-22 22:53:34 -0400

    Update errorMessages.js

--- server/src/utils/errorMessages.js
+++ server/src/utils/errorMessages.js
@@ -3,5 +3,5 @@ module.exports = {
   badRequest: 'Malformed request',
   forbidden: 'You are not authorized to access this resource',
   forbiddenAction: 'You are not authorized to take this action',
-  invalidCurrentPassword: 'Invalid current password'
+  invalidCurrentPassword: 'Invalid current passworddd'
 };
