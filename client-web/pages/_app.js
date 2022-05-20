461668caf229da4adc87702ffadff6dffcdc5fc3
Simon Davies <slndavies@gmail.com>
2022-05-19 20:36:16 -0400

    Update _app.js

--- client-web/pages/_app.js
+++ client-web/pages/_app.js
@@ -1,23 +1,17 @@
 import App from "next/app";
 import "../styles/globals.scss";
 import { appWithTranslation } from "next-i18next";
-// import { loadStripe } from '@stripe/stripe-js';
-// import { Elements } from '@stripe/react-stripe-js';
 import { Provider as AuthProvider } from "../context/AuthContext";
 import { Provider as UserProvider } from "../context/UserContext";
 import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }) {
-  // const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
-
   return (
     <AuthProvider>
       <UserProvider>
-        {/* <Elements stripe={stripePromise}> */}
         <Layout brandName="Hello Brand">
           <Component {...pageProps} />
         </Layout>
-        {/* </Elements> */}
       </UserProvider>
     </AuthProvider>
   );
