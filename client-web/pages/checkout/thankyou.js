74568d7a9e1e71fd3a71c668af3bf98e7975a159
Simon Davies <slndavies@gmail.com>
2022-06-02 08:44:24 -0400

    added timeout to refresh cart on thank you page

--- client-web/pages/checkout/thankyou.js
+++ client-web/pages/checkout/thankyou.js
@@ -1,8 +1,19 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
+import { useEffect, useContext } from "react";
+import { Context as AuthContext } from "../../context/AuthContext";
 
 export default function SignOut() {
   const { t } = useTranslation("common");
+  const {
+    state: { auth }, getAuth,
+  } = useContext(AuthContext);
+
+  useEffect(() => {
+    setTimeout(() => {
+      getAuth();
+    }, 1000)
+  }, []);
 
   return (
     <div>
