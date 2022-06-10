6dc1378f773d6b72ce173e24c4e3d9c1545045f7
Simon Davies <slndavies@gmail.com>
2022-06-10 08:31:27 -0400

    Update Loader.js

--- client-web/components/Loader.js
+++ client-web/components/Loader.js
@@ -1,17 +1,16 @@
 import React from "react";
-// import { ScaleLoader } from "react-spinners";
+import { ScaleLoader } from "react-spinners";
 
 const Loader = () => {
   return (
     <div className="sweet-loading">
-      {/* <ScaleLoader
+      <ScaleLoader
         height={10}
         width={4}
         color={"#4E5D6C"}
         margin="2px"
         loading
-      /> */}
-      ... Loading ...
+      />
     </div>
   );
 };
