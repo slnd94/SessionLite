942ccfdb001eb1b4d9ae51763bb77464a1ba79f2
Simon Davies <slndavies@gmail.com>
2022-06-23 11:54:47 -0400

    attach userip in next

---
+++ client-web/utils/ipUtils.js
@@ -0,0 +1,23 @@
+export const getUserIP = (req) => {
+  let ip;
+  if (req.headers["x-forwarded-for"]) {
+    ip = req.headers["x-forwarded-for"].split(",")[0];
+  } else if (req.headers["x-real-ip"]) {
+    ip = req.connection.remoteAddress;
+  } else {
+    ip = req.connection.remoteAddress;
+  }
+  if (isValidIP(ip)) {
+    return ip;
+  } else {
+    return null;
+  }
+};
+
+const isValidIP = (str) => {
+  // Regular expression to check if string is a IP address
+  const regexExp =
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+
+  return regexExp.test(str);
+};
