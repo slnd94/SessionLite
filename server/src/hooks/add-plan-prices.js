2b2f046eab9177499f6e4584b53e79caa4a274eb
Simon Davies <slndavies@gmail.com>
2022-07-23 23:17:38 -0400

    Update add-plan-prices.js

--- server/src/hooks/add-plan-prices.js
+++ server/src/hooks/add-plan-prices.js
@@ -11,21 +11,21 @@ module.exports = (options = {}) => {
       console.log("🚀 ~ file: add-plan-prices.js ~ line 11 ~ return ~ plan", plan)
 
       //// ONLINE
-      const paddleResponse = await api({
-        method: "get",
-        url: `${context.app.get("paddleApiBaseUrl")}/prices\?product_ids=${plan.paddle.productId}\&customer_country=${context.params.userCountry.code}`,
-      });
-      const paddleProduct = paddleResponse.data.response.products.find(
-        (product) => product.product_id === plan.paddle.productId && product
-      );
+      // const paddleResponse = await api({
+      //   method: "get",
+      //   url: `${context.app.get("paddleApiBaseUrl")}/prices\?product_ids=${plan.paddle.productId}\&customer_country=${context.params.userCountry.code}`,
+      // });
+      // const paddleProduct = paddleResponse.data.response.products.find(
+      //   (product) => product.product_id === plan.paddle.productId && product
+      // );
 
-      context.result = {
-        ...context.result,
-        subscription: {
-          ...paddleProduct.subscription,
-          currency: paddleProduct.currency
-        }
-      }
+      // context.result = {
+      //   ...context.result,
+      //   subscription: {
+      //     ...paddleProduct.subscription,
+      //     currency: paddleProduct.currency
+      //   }
+      // }
       /// ONLINE
 
       ///  OFFLINE
@@ -47,45 +47,45 @@ module.exports = (options = {}) => {
 
       /// ONLINE
       // concatenated string of planIds example "28237,383763,29272,29280"
-      const planIds = plans.map((plan) => plan.paddle.productId).join();
-      const paddleResponse = await api({
-        method: "get",
-        url: `${context.app.get("paddleApiBaseUrl")}/prices\?product_ids=${planIds}\&customer_country=${context.params.userCountry.code}`,
-      });
-      const paddleProducts = paddleResponse.data.response.products;
+      // const planIds = plans.map((plan) => plan.paddle.productId).join();
+      // const paddleResponse = await api({
+      //   method: "get",
+      //   url: `${context.app.get("paddleApiBaseUrl")}/prices\?product_ids=${planIds}\&customer_country=${context.params.userCountry.code}`,
+      // });
+      // const paddleProducts = paddleResponse.data.response.products;
 
-      context.result.data = plans.map((plan) => {
-        const product = paddleProducts.find(
-          (product) => product.product_id === plan.paddle.productId && product
-        );
-        return {
-          ...(product
-            ? {
-                subscription: {
-                  ...product.subscription,
-                  currency: product.currency
-                }
-              }
-            : {}),
-          ...plan,
-        };
-      });
+      // context.result.data = plans.map((plan) => {
+      //   const product = paddleProducts.find(
+      //     (product) => product.product_id === plan.paddle.productId && product
+      //   );
+      //   return {
+      //     ...(product
+      //       ? {
+      //           subscription: {
+      //             ...product.subscription,
+      //             currency: product.currency
+      //           }
+      //         }
+      //       : {}),
+      //     ...plan,
+      //   };
+      // });
       /// ONLINE
 
 
       ///  OFFLINE
-      // context.result.data = plans.map((plan) => {
-      //   return {
-      //     subscription: {
-      //       price: {
-      //         gross: 0.00
-      //       },
-      //       currency: "CAD",
-      //       interval: "month"
-      //     },
-      //     ...plan,
-      //   };
-      // });
+      context.result.data = plans.map((plan) => {
+        return {
+          subscription: {
+            price: {
+              gross: 0.00
+            },
+            currency: "CAD",
+            interval: "month"
+          },
+          ...plan,
+        };
+      });
       ///  OFFLINE
     }
 
