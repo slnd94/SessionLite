a7b50d2808acde3c6c392f5391975a21e7012a3e
Simon Davies <slndavies@gmail.com>
2022-09-26 15:35:55 -0400

    added healthz service for api health check

---
+++ server/src/services/healthz/healthz.class.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+exports.Healthz = class Healthz {
+  constructor (options) {
+    this.options = options || {};
+  }
+
+  async find (params) {
+    return [];
+  }
+};
