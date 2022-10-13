9dc9e30b75d8de7dfd7571a461b72591a794edf2
Simon Davies <slndavies@gmail.com>
2022-10-13 14:32:33 -0400

    react hook form and react native elements inputs

--- client-mobile/src/components/auth/SignInForm.js
+++ client-mobile/src/components/auth/SignInForm.js
@@ -1,15 +1,115 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Input } from '@rneui/themed';
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, Alert } from "react-native";
+import { Input, Button } from "@rneui/themed";
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { useTranslation } from "react-i18next";
 
-const SignInForm = () => {
+const SignInForm = ({ onSubmit, processing }) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { t } = useTranslation();
+
+  const formRules = {
+    email: {
+      required: t("auth.Email is required"),
+      pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        message: "invalid email address",
+      },
+    },
+    password: {
+      required: t("auth.Password is required"),
+      minLength: {
+        value: 6,
+        message: t("auth.Password must have at least <num> characters"),
+      },
+    },
+  };
+
   return (
-    <View>
-      {/* <Input> */}
+    <View style={styles.container}>
+      <Controller
+        control={control}
+        rules={formRules.email}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            inputStyle={styles.input}
+            label={t("auth.Email")}
+            autoCapitalize="none"
+            autoComplete="off"
+            autoCorrect={false}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="email"
+      />
+      {errors.email && (
+        <View style={styles.inputValidation}>
+          <Text style={styles.inputValidationText}>{errors.email.message}</Text>
+        </View>
+      )}
+
+      <Controller
+        control={control}
+        rules={formRules.password}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            style={styles.input}
+            label={t("auth.Password")}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry={true}
+          />
+        )}
+        name="password"
+      />
+      {errors.password && (
+        <View style={styles.inputValidation}>
+          <Text style={styles.inputValidationText}>{errors.password.message}</Text>
+        </View>
+      )}
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
-  )
-}
+  );
+};
 
-export default SignInForm
+export default SignInForm;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: "stretch",
+    // borderWidth: 4,
+    // borderColor: "#7033f4",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
+    // margin: 30
+    // justifyContent: "flex-end"
+  },
+  input: {
+    flex: 1,
+    // width: 500,
+    // borderWidth: 4,
+    // borderColor: "#f43333",
+    // alignSelf: "stretch"
+  },
+  inputValidation: {
+    marginBottom: 30
+  },
+  inputValidationText: {
+    color: "#f43333"
+  }
+});
--- client-mobile/src/screens/SignIn.js
+++ client-mobile/src/screens/SignIn.js
@@ -4,6 +4,7 @@ import React, { useEffect, useContext } from "react";
 import { Context as AuthContext } from "../context/AuthContext";
 import { useTranslation } from "react-i18next";
 import SignInForm from "../components/auth/SignInForm";
+import Spacer from "../components/Spacer";
 
 const SignIn = ({ navigation }) => {
   const {
@@ -15,10 +16,29 @@ const SignIn = ({ navigation }) => {
   const { t } = useTranslation();
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text h1>Sign In ({t("auth.Reset Your Password")})</Text>
-      <Text>Status is {auth?.status}</Text>
-      <SignInForm />
+    <View style={{ margin: 30, flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text h3>{t("auth.Sign In")}</Text>
+      {/* <Text>Status is {auth?.status}</Text> */}
+      <Spacer />
+      <SignInForm
+        // processing={processing}
+        onSubmit={async (data) => {
+          console.log("🚀 ~ file: SignIn.js ~ line 24 ~ onSubmit={ ~ data", data)
+        //   setProcessing(true);
+        //   const request = await signin(data);
+        //   if (request.success) {
+        //     setProcessing(false);
+        //     if (redirect) {
+        //       router.push({
+        //         pathname: redirect,
+        //         query: redirectQuery || {},
+        //       });
+        //     }
+        //   } else {
+        //     setProcessing(false);
+        //   }
+        }}
+      />
     </View>
   );
 };
